c3a7a7afd906e172e66b6ee4a49bf0be2bd9a0e0
Graham Watson <graham@phantomwatson.com>
2016-02-19 20:37:50 -0500

    Customize select and input fields

--- config/bootstrap_form.php
+++ config/bootstrap_form.php
@@ -1,8 +1,11 @@
 <?php
 return [
-	'error' => '<div class="alert alert-danger">{{content}}</div>',
-	'inputContainer' => '<div class="form-group {{type}}{{required}}">{{content}}</div>',
-	'inputContainerError' => '<div class="form-group has-error has-feedback {{type}}{{required}}">{{content}}{{error}}</div>'
+    'error' => '<div class="alert alert-danger">{{content}}</div>',
+    'inputContainer' => '<div class="form-group {{type}}{{required}}">{{content}}</div>',
+    'inputContainerError' => '<div class="form-group has-error has-feedback {{type}}{{required}}">{{content}}{{error}}</div>',
+    'select' => '<select class="form-control" name="{{name}}"{{attrs}}>{{content}}</select>',
+    'selectMultiple' => '<select class="form-control" name="{{name}}[]" multiple="multiple"{{attrs}}>{{content}}</select>',
+    'input' => '<input class="form-control" type="{{type}}" name="{{name}}"{{attrs}}>',
 
 	/* defaults
 	'button' => '<button{{attrs}}>{{text}}</button>',
