0f533397da95c509270acbec498e7cf4921a6965
Graham Watson <graham@phantomwatson.com>
2016-02-19 21:00:37 -0500

    Add /admin/payments/add

--- src/Controller/Admin/PaymentsController.php
+++ src/Controller/Admin/PaymentsController.php
@@ -35,4 +35,41 @@ class PaymentsController extends AppController
             'payments' => $payments
         ]);
     }
+
+    public function add()
+    {
+        $payment = $this->Payments->newEntity();
+        if ($this->request->is('post')) {
+            $this->request->data['admin_adder_id'] = $this->Auth->user('id');
+            $this->request->data['postback'] = '';
+            $payment = $this->Payments->patchEntity($payment, $this->request->data());
+            $errors = $payment->errors();
+            if (empty($errors) && $this->Payments->save($payment)) {
+                $this->Flash->success('Payment record added');
+                return $this->redirect([
+                    'action' => 'index'
+                ]);
+            }
+            $this->Flash->error('There was an error adding a new payment record');
+        }
+
+        $usersTable = TableRegistry::get('Users');
+        $users = $usersTable->find('list')->order(['name' => 'ASC']);
+
+        $membershipLevelsTable = TableRegistry::get('MembershipLevels');
+        $results = $membershipLevelsTable->find('all')
+            ->select(['id', 'name', 'cost'])
+            ->order(['cost' => 'ASC']);
+        $membershipLevels = [];
+        foreach ($results as $membershipLevel) {
+            $membershipLevels[$membershipLevel->id] = $membershipLevel->name.' ($'.number_format($membershipLevel->cost).')';
+        }
+
+        $this->set([
+            'membershipLevels' => $membershipLevels,
+            'pageTitle' => 'Add a New Payment Record',
+            'payment' => $payment,
+            'users' => $users
+        ]);
+    }
 }
---
+++ src/Template/Admin/Payments/add.ctp
@@ -0,0 +1,35 @@
+<p>
+    <?= $this->Html->link(
+        '<span class="glyphicon glyphicon-arrow-left"></span> Back to Payment Records',
+        [
+            'prefix' => 'admin',
+            'controller' => 'Payments',
+            'action' => 'index'
+        ],
+        [
+            'class' => 'btn btn-default',
+            'escape' => false
+        ]
+    ) ?>
+</p>
+
+<p>
+    You will need to manually add a payment record if someone pays for membership
+    by a method <em>other than</em> through the MACC website, such as with a check. Please include
+    information about how this payment was received in the <strong>notes</strong> section.
+</p>
+
+<?= $this->Form->create($payment) ?>
+
+<?= $this->Form->input('user_id') ?>
+
+<?= $this->Form->input('membership_level_id') ?>
+
+<?= $this->Form->input('notes') ?>
+
+<?= $this->Form->button(
+    'Add Payment Record',
+    ['class' => 'btn btn-primary']
+) ?>
+
+<?= $this->Form->end() ?>
