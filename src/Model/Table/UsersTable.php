9d8fd490e56b2701691e45133ba614cf0bd58758
Graham Watson <graham@phantomwatson.com>
2016-02-26 22:23:12 -0500

    Add phpdoc comment

--- src/Model/Table/UsersTable.php
+++ src/Model/Table/UsersTable.php
@@ -146,6 +146,13 @@ class UsersTable extends Table
         return $user->first()->id;
     }
 
+    /**
+     * Finds all users with memberships that have not expired or been canceled
+     *
+     * @param Query $query
+     * @param array $options
+     * @return Query
+     */
     public function findMembers(Query $query, array $options)
     {
         return $query->matching('Memberships', function ($q) {
