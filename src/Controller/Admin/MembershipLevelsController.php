511578877b62c7ebb92cd8d1e5044f95d79a62b6
Graham Watson <graham@phantomwatson.com>
2016-03-02 19:43:07 -0500

    Fix incorrect request type

--- src/Controller/Admin/MembershipLevelsController.php
+++ src/Controller/Admin/MembershipLevelsController.php
@@ -44,7 +44,7 @@ class MembershipLevelsController extends AppController
     public function edit($membershipLevelId)
     {
         $membershipLevel = $this->MembershipLevels->get($membershipLevelId);
-        if ($this->request->is('post')) {
+        if ($this->request->is('put')) {
             $membershipLevel = $this->MembershipLevels->patchEntity($membershipLevel, $this->request->data);
             if ($this->MembershipLevels->save($membershipLevel)) {
                 $this->Flash->success('Membership level updated');
