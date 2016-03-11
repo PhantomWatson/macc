cc0a73754bae2e903537acfe9bc956ed4e5b24e1
Graham Watson <graham@phantomwatson.com>
2016-03-10 22:50:19 -0500

    add testToggleAutoRenewalAuth() and Unauth()

--- tests/TestCase/Controller/MembershipsControllerTest.php
+++ tests/TestCase/Controller/MembershipsControllerTest.php
@@ -3,6 +3,7 @@ namespace App\Test\TestCase\Controller;
 
 use App\Controller\MembershipsController;
 use App\Test\Fixture\UsersFixture;
+use Cake\ORM\TableRegistry;
 use Cake\TestSuite\IntegrationTestCase;
 
 /**
@@ -21,6 +22,16 @@ class MembershipsControllerTest extends IntegrationTestCase
         'app.memberships',
         'app.users'
     ];
+
+    public function setMemberSession()
+    {
+        $usersFixture = new UsersFixture();
+        $this->session([
+            'Auth' => [
+                'User' => $usersFixture->records[0]
+            ]
+        ]);
+    }
 
     public function setNonMemberSession()
     {
@@ -101,6 +112,44 @@ class MembershipsControllerTest extends IntegrationTestCase
         $this->get([
             'controller' => 'Memberships',
             'action' => 'purchaseComplete'
+        ]);
+        $this->assertRedirect([
+            'controller' => 'Users',
+            'action' => 'login'
+        ]);
+    }
+
+    public function testToggleAutoRenewalAuth()
+    {
+        $this->setMemberSession();
+
+        $expected = 1;
+        $this->get([
+            'controller' => 'Memberships',
+            'action' => 'toggleAutoRenewal',
+            $expected
+        ]);
+        $membershipsTable = TableRegistry::get('Memberships');
+        $result = $membershipsTable->get(1)->auto_renew;
+        $this->assertEquals($expected, $result);
+
+        $expected = 0;
+        $this->get([
+            'controller' => 'Memberships',
+            'action' => 'toggleAutoRenewal',
+            $expected
+        ]);
+        $membershipsTable = TableRegistry::get('Memberships');
+        $result = $membershipsTable->get(1)->auto_renew;
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testToggleAutoRenewalUnauth()
+    {
+        $this->get([
+            'controller' => 'Memberships',
+            'action' => 'toggleAutoRenewal',
+            1
         ]);
         $this->assertRedirect([
             'controller' => 'Users',
