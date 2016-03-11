3b6f47d62591257f13fd08b5a2411def64b65fc7
Graham Watson <graham@phantomwatson.com>
2016-03-10 21:17:44 -0500

    Remove custom phpunit notifications
    Success notification was being shown regardless of error/success status,
    plus the default notifications work fine.

--- gulpfile.js
+++ gulpfile.js
@@ -37,24 +37,10 @@ gulp.task('php_cs', function() {
 		.pipe(phpcs.reporter('log'));
 });
 
-function testNotification(status, pluginName, override) {
-    var options = {
-        title:   ( status == 'pass' ) ? 'Tests Passed' : 'Tests Failed',
-        message: ( status == 'pass' ) ? '\n\nAll tests have passed!\n\n' : '\n\nOne or more tests failed...\n\n',
-        icon:    __dirname + '/node_modules/gulp-' + pluginName +'/assets/test-' + status + '.png'
-    };
-    options = _.merge(options, override);
-    return options;
-}
-
 gulp.task('php_unit', function() {
     gulp.src('phpunit.xml')
-        .pipe(phpunit('', {notify: true}))
-        .on('error', notify.onError(testNotification('fail', 'phpunit')))
-        .pipe(notify(testNotification('pass', 'php_unit')));
+        .pipe(phpunit('', {notify: true}));
 });
-
-
 
 /**************
  * Javascript *
