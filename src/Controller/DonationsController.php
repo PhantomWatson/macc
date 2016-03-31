8880c755ea6008b2d7e9e911d2e8c50e1e687fd7
Graham Watson <graham@phantomwatson.com>
2016-03-31 11:30:37 -0400

    Fill in donation metadata with (anonymous) if user unknown

--- src/Controller/DonationsController.php
+++ src/Controller/DonationsController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 use Cake\Core\Configure;
+use Cake\Network\Exception\ForbiddenException;
 
 class DonationsController extends AppController
 {
@@ -21,6 +22,14 @@ class DonationsController extends AppController
         }
     }
 
+    public function beforeFilter(\Cake\Event\Event $event)
+    {
+         /* Prevent Security component from stripping out "unknown fields"
+          * from AJAX request to completeDonation and causing errors
+          * http://book.cakephp.org/3.0/en/controllers/components/security.html#form-tampering-prevention */
+         $this->Security->config('unlockedActions', ['completeDonation']);
+    }
+
     public function donate()
     {
         $this->set([
@@ -30,7 +39,40 @@ class DonationsController extends AppController
 
     public function completeDonation()
     {
-        // No validation or recording currently takes place for donations
+        // Validate amount
+        $amount = $this->request->data('amount');
+        if (! is_numeric($amount)) {
+            throw new ForbiddenException('Donation amount must be numeric');
+        } elseif ($amount < 1) {
+            throw new ForbiddenException('Donation must be at least one dollar');
+        }
+
+        $metadata = [];
+        if ($this->Auth->user('id')) {
+            $metadata['Donor name'] = $this->Auth->user('name');
+            $metadata['Donor email'] = $this->Auth->user('email');
+        } else {
+            $metadata['Donor name'] = '(anonymous)';
+            $metadata['Donor email'] = '(unknown)';
+        }
+
+        // Create the charge on Stripe's servers - this will charge the user's card
+        $apiKey = Configure::read('Stripe.Secret');
+        \Stripe\Stripe::setApiKey($apiKey);
+        try {
+            $description = 'Donation to MACC of $'.number_format($amount, 2);
+            $charge = \Stripe\Charge::create([
+                'amount' => $amount * 100, // amount in cents
+                'currency' => 'usd',
+                'source' => $this->request->data('stripeToken'),
+                'description' => $description,
+                'metadata' => $metadata,
+                'receipt_email' => $this->request->data('email')
+            ]);
+        } catch(\Stripe\Error\Card $e) {
+            throw new ForbiddenException('The provided credit card has been declined');
+        }
+
         $this->viewBuilder()->layout('json');
         $this->set([
             '_serialize' => ['retval'],
