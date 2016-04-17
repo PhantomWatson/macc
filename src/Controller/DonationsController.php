9ddedacbfbf146ae0ea641509c0ef853a0ecd0be
Graham Watson <graham@phantomwatson.com>
2016-04-17 19:08:39 -0400

    Fix line indentation

--- src/Controller/DonationsController.php
+++ src/Controller/DonationsController.php
@@ -27,9 +27,9 @@ class DonationsController extends AppController
          /* Prevent Security component from stripping out "unknown fields"
           * from AJAX request to completeDonation and causing errors
           * http://book.cakephp.org/3.0/en/controllers/components/security.html#form-tampering-prevention */
-         if (Configure::read('forceSSL')) {
+        if (Configure::read('forceSSL')) {
             $this->Security->config('unlockedActions', ['completeDonation']);
-         }
+        }
     }
 
     public function donate()
