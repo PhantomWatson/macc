998432b3cea17ff02b9d2dfe2bff954e9135d8ef
Graham Watson <graham@phantomwatson.com>
2018-07-29 17:34:35 -0400

    Whoops, fix which database the default username was set for

--- config/app.php
+++ config/app.php
@@ -269,7 +269,7 @@ $config = [
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => env('DATABASE_USERNAME', 'root'),
+            'username' => env('DATABASE_USERNAME'),
             'password' => env('DATABASE_PASSWORD'),
             'database' => env('DATABASE_NAME'),
             /*
@@ -312,7 +312,7 @@ $config = [
             'persistent' => false,
             'host' => 'localhost',
             //'port' => 'nonstandard_port_number',
-            'username' => env('TEST_DATABASE_USERNAME'),
+            'username' => env('TEST_DATABASE_USERNAME', 'root'),
             'password' => env('TEST_DATABASE_PASSWORD'),
             'database' => env('TEST_DATABASE_NAME'),
             'encoding' => 'utf8',
